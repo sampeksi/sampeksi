dfab0a3c308107525b01a9af130c82f4b2b05793
Sampo Suokuisma <sampo.suokuisma@tuni.fi>
2022-09-03 17:28:46 +0300

    lampotiloja

---
+++ student/02/temperature/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int lampo = 0;
+    cout << "Enter a temperature: ";
+    cin >> lampo;
+
+    cout << lampo << " degrees Celsius is " << lampo * 1.8 + 32 << " degrees Fahrenheit" << endl;
+
+    cout << lampo << " degrees Celsius is " << (lampo - 32) / 1.8 << " degrees Fahrenheit" << endl;
+
+    return 0;
+}
