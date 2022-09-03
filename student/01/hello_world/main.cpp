aed9690296796b26d08a893b7d5d46bd9a092729
Sampo Suokuisma <sampo.suokuisma@tuni.fi>
2022-09-03 16:56:15 +0300

    tyypitys

---
+++ student/01/hello_world/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+    cout << "Nollalla jako: " << 1/0 << endl;
+}
---
+++ student/01/tyypitys/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+
+    bool b = 1 + 2;
+    cout << b << endl;
+
+}
