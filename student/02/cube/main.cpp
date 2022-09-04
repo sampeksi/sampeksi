83b6a00d4fc36951101685878ebeab2fd51f0194
Sampo Suokuisma <sampo.suokuisma@tuni.fi>
2022-09-04 17:15:06 +0300

    kuutio

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    cout << "Enter a number: ";
+    cin >> number;
+
+    if ((number * number * number) > number) {
+        cout << "The cube of " << number << " is " << number * number * number <<"." <<endl;
+    } else {
+        cout << "Error! The cube of " << number << " is not " << number * number * number << "." <<endl;
+    }
+
+
+    return 0;
+}
