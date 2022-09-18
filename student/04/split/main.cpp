0bbda3c87a6e1b120f2980d489ee21886f366609
Sampo Suokuisma <sampo.suokuisma@tuni.fi>
2022-09-18 21:06:54 +0300

    split

--- student/04/split/main.cpp
+++ student/04/split/main.cpp
@@ -2,10 +2,61 @@
 #include <string>
 #include <vector>
 
+using namespace std;
 
-// TODO: Implement split function here
-// Do not change main function
+vector<string> split(string line, char sep, bool spaces = false)
+{
+    int length = line.length();
+    if (spaces == true) {
+        vector<string> prints;
+        string chars = "";
 
+        for (int i = 0; i+1 < length; ++i) {
+            if (line.at(i) == sep) {
+                if (chars != "") {
+                    prints.push_back(chars);
+                    chars = "";
+                }
+            } else {
+                chars += line.at(i);
+            }
+            if (i+2 == length) {
+                if (line.at(i+1) != sep) {
+                    chars += line.at(i+1);}
+                if (chars != "") {
+                    prints.push_back(chars);}
+
+            }
+        } return prints;
+    } else {
+        vector<string> prints;
+        string chars = "";
+        if (line.at(0) == sep) {
+            prints.push_back("");
+        }
+        for (int i = 0; i+1 < length; ++i) {
+            if (line.at(i) == sep) {
+                if (chars != "") {
+                prints.push_back(chars);
+                chars = "";}
+
+                if (line.at(i+1) == sep) {
+                    prints.push_back("");
+                }
+            } else {
+                chars += line.at(i);
+        } if (i+2 == length) {
+                if (line.at(i+1) == sep) {
+                    prints.push_back(chars);
+                    prints.push_back("");
+                } else {
+                    chars = line.at(i+1);
+                    prints.push_back(chars);
+                }
+            }
+        }
+    return prints;}
+}
 
 int main()
 {
