f3f9466ba8066baa46821e0b7ec86327acb622b4
Sampo Suokuisma <sampo.suokuisma@tuni.fi>
2022-11-23 10:34:17 +0200

    bmi2

--- student/11/bmi/mainwindow.cpp
+++ student/11/bmi/mainwindow.cpp
@@ -23,17 +23,22 @@ void MainWindow::on_countButton_clicked()
     double weigth = w.toDouble();
     double height = h.toDouble();
 
+    if (h == "" or height == 0) {
+        ui->infoTextBrowser->setText("Cannot count.");
+        ui->resultLabel->setText("");
+    } else {
     double bmi = weigth / ((height / 100)*(height /100));
 
     QString result = QString::number(bmi, 'f', 4);
     ui->resultLabel->setText(result);
 
     if (bmi < 18.5) {
-        ui->textBrowser->setText("You are underweight.");
+        ui->infoTextBrowser->setText("You are underweight.");
     } else if (bmi > 25) {
-        ui->textBrowser->setText("You are overweight.");
+        ui->infoTextBrowser->setText("You are overweight.");
     } else {
-        ui->textBrowser->setText("Your weight is normal.");
+        ui->infoTextBrowser->setText("Your weight is normal.");
+    }
     }
 }
 
